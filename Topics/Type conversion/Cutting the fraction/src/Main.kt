830ef3169d390b966a7258472a3d7af5b5aa049c
JoJo <buyongjo@gmail.com>
2022-04-06 21:39:55 +0900

    theory: basic types and conversion

---
+++ Topics/Type conversion/Cutting the fraction/src/Main.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val value = scanner.nextDouble()
+
+    println(value.toLong())
+}
---
+++ Topics/Type conversion/Simple converter/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // write your code here
+    val value = readln()
+
+    println(value.toInt())
+    println(value.toDouble())
+    println(value.toBoolean())
+}
